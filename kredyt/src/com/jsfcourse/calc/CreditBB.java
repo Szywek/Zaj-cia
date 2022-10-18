2e950e911dcd2ecb150273ef898865b8b933d9f4
Szymon Warszawski <93082471+Szywek@users.noreply.github.com>
2022-10-18 02:33:45 +0200

    added PrimeFaces

--- kredyt/src/com/jsfcourse/calc/CreditBB.java
+++ kredyt/src/com/jsfcourse/calc/CreditBB.java
@@ -1,28 +1,27 @@
 package com.jsfcourse.calc;
 
-import javax.inject.Inject;
-import javax.inject.Named;
 import javax.enterprise.context.RequestScoped;
-import javax.enterprise.context.SessionScoped;
 import javax.faces.application.FacesMessage;
 import javax.faces.context.FacesContext;
+import javax.inject.Inject;
+import javax.inject.Named;
 
 @Named
 @RequestScoped
 //@SessionScoped
 
 public class CreditBB {
-	
+
 	private String x;
 	private String y;
 	private String z;
-	
+
 	private Double result;
 
 	@Inject
 	FacesContext ctx;
 
-	
+
 
 	public String getX() {
 		return x;
@@ -55,18 +54,19 @@ public class CreditBB {
 	public void setResult(Double result) {
 		this.result = result;
 	}
-	
+
 
 	public boolean doTheMath() {
 		try {
 			double x = Double.parseDouble(this.x);
 			double y = Double.parseDouble(this.y);
 			double z = Double.parseDouble(this.z);
-			
+
 			double w = (x / (y*12));
 			result = w + (w*(z/100));
 
-			ctx.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Operacja wykonana", null));
+			ctx.addMessage(null,
+					new FacesMessage(FacesMessage.SEVERITY_INFO, "Operacja wykonana", null));
 			return true;
 		} catch (Exception e) {
 			ctx.addMessage(null,
@@ -75,12 +75,13 @@ public class CreditBB {
 		}
 	}
 
-	
+
 	public String calc() {
 		if (doTheMath()) {
-			return "showresult";
+			ctx.addMessage(null,
+					new FacesMessage(FacesMessage.SEVERITY_INFO, "Wynik: " + result, null));
 		}
 		return null;
 	}
-
+	
 }
